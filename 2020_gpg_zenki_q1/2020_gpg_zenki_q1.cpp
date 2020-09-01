7d3f1d3c74fc3a07d421b9405c290b8f41072d59
momokaaoki <momokaaoki0216@gmail.com>
2020-09-01 10:23:16 +0900

    プロジェクト ファイルを追加します。

---
+++ 2020_gpg_zenki_q1/2020_gpg_zenki_q1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //1~100までの繰り返し
+    for (int i = 1; i < 101; ++i) {
+
+        if ((i % 3 == 0) && (i % 5 == 0)) {     //3と5で割り切れる
+            cout << "FizzBuzz" << endl;
+        }
+        else if (i % 5 == 0) {                  //5で割り切れる
+            cout << "Buzz" << endl;
+        }
+        else if (i % 3 == 0) {                  //3で割り切れる
+            cout << "Fizz" << endl;
+        }
+        else {                                  //それ以外の場合               
+            cout << i << endl;
+        }
+    }
+}
